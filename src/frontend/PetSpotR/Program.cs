86d716713f6d88ab5c993a271e2f477e0eaa7554
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-01-14 16:06:06 -0800

    Remove references to unused pages

--- src/frontend/PetSpotR/Program.cs
+++ src/frontend/PetSpotR/Program.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using PetSpotR.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
 
