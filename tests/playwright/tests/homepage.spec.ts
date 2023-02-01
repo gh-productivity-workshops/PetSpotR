bb1a733ff34a4572bbb5567105970187405d384f
Aaron Crawfis <Aaron.Crawfis@microsoft.com>
2023-01-31 17:59:49 -0800

    Change Link to Button

--- tests/playwright/tests/homepage.spec.ts
+++ tests/playwright/tests/homepage.spec.ts
@@ -11,7 +11,7 @@ test('get started link', async ({ page }) => {
   await page.goto('http://app.9e5524d3a2474ae58276.westus3.aksapp.io/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'I lost my pet' }).click();
+  await page.getByRole('button', { name: 'I lost my pet' }).click();
 
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*lost/);
