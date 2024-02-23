65fe35d60b449ceb5aa4742f7b388499d57fe171
Michelle Mannering <mishmanners@github.com>
2024-02-23 12:30:13 +1100

    Add back in

--- src/frontend/PetSpotR/Data/PetModel.cs
+++ src/frontend/PetSpotR/Data/PetModel.cs
@@ -11,7 +11,6 @@ namespace PetSpotR.Models
         public string ID { get; set; }
         public string State { get; set; }
         public List<string> Images { get; set; }
-        public string Location { get; set; }
 
         // Constructor
         public PetModel()
@@ -23,7 +22,6 @@ namespace PetSpotR.Models
             ID = Guid.NewGuid().ToString();
             State = "new";
             Images = new();
-            Location = "";
         }
 
         public async Task SavePetStateAsync(DaprClient daprClient, string storeName)
