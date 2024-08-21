23ed63b66e7951f3d81fc6461fad53c50479f806
Michelle "MishManners®™" Duke <36594527+mishmanners@users.noreply.github.com>
2024-08-21 12:03:03 +1000

    Fix failing test

--- tests/playwright/tests/homepage.spec.ts
+++ tests/playwright/tests/homepage.spec.ts
@@ -1,17 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-// Helper function to navigate to the homepage and click the "continue" button
-test.beforeEach(async ({ page }) => {
-  await page.goto('/');
-  await page.click('button:has-text("Continue")');
-});
-
 test('has title', async ({ page }) => {
+  await page.goto('/');
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/PetSpotR/);
 });
 
 test('get started link', async ({ page }) => {
+  await page.goto('/');
   // Click the get started link.
   await page.getByRole('button', { name: 'I lost my pet' }).click();
   // Expects the URL to contain intro.
@@ -29,7 +25,7 @@ test('fill out and submit lost pet form', async ({ page }) => {
   await page.selectOption('#lostlocation', 'Melbourne');
   await page.fill('#ownername', 'John Doe');
   await page.fill('#owneremail', 'john.doe@example.com');
-  await page.setInputFiles('#petimage', 'tests/playwright/fixtures/buddy.jpg');
+  await page.setInputFiles('#petimage', 'fixtures/buddy.jpg');
 
   // Submit the form
   await page.click('button:has-text("Submit")');
