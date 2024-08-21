aaa9e6f979e9b6d3e8c6fd54c71c5a35f7682e8b
Marais Rossouw <me@marais.co>
2024-08-21 11:27:54 +1000

    chore: playwright should take its baseUrl from a config

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -35,8 +35,9 @@ const config: PlaywrightTestConfig = {
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
+
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: process.env.TEST_URL ?? `http://localhost:5114/`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -96,12 +97,14 @@ const config: PlaywrightTestConfig = {
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 };
+
+if (process.env.TEST_URL?.includes('localhost')) {
+  /* Run your local dev server before starting the tests */
+  config.webServer = {
+    command: '../../scripts/dev',
+    url: 'http://localhost:5114/'
+  };
+}
 
 export default config;
